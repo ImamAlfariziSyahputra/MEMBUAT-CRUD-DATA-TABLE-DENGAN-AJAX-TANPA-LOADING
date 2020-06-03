50846cd4c142db005b2dca87df59e3ecc077ce9d
Imam Alfarizi Syahputra <63343105+ImamAlfariziSyahputra@users.noreply.github.com>
2020-06-03 13:03:23 +0700

    Update get_data_all.php
    memperbaiki kutip pada $row[];

--- starbhak-ajax2Copy/get_data_all.php
+++ starbhak-ajax2Copy/get_data_all.php
@@ -9,12 +9,12 @@ $prepare1->bind_param('i', $id);
 $prepare1->execute();
 $res1 = $prepare1->get_result();
 while ($row = $res1->fetch_assoc()) {
-    $h['id'] = $row['id'];
-    $h['nama_siswa'] = $row['nama_siswa'];
-    $h['alamat'] = $row['alamat'];
-    $h['jurusan'] = $row['jurusan'];
-    $h['jenis_kelamin'] = $row['jenis_kelamin'];
-    $h['tgl_masuk'] = $row['tgl_masuk'];
+    $h['id'] = $row["id"];
+    $h['nama_siswa'] = $row["nama_siswa"];
+    $h['alamat'] = $row["alamat"];
+    $h['jurusan'] = $row["jurusan"];
+    $h['jenis_kelamin'] = $row["jenis_kelamin"];
+    $h['tgl_masuk'] = $row["tgl_masuk"];
 }
 
 echo json_encode($h);
